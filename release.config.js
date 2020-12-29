6b054dd9c84428a3af5323f06ffe2b67a9254dc0
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-12-29 14:13:21 -0600

    fix(release): remap releaes types
    Update the release types to inclued more types so the release notes
    include something

--- release.config.js
+++ release.config.js
@@ -1,28 +1,55 @@
 'use strict'
 
+const COMMIT_TYPES = new Map([
+  ['feat', 'Features']
+, ['fix', 'Bug Fixes']
+, ['perf', 'Performance Improvements']
+, ['revert', 'Reverts']
+, ['doc', 'Documentation']
+, ['style', 'Style']
+, ['lint', 'Lint']
+, ['refactor', 'Code Refactoring']
+, ['test', 'Tests']
+, ['build', 'Build System']
+, ['ci', 'Continuous Integration']
+, ['chore', 'Chores']
+, ['default', 'Miscellaneous']
+])
+
+function typeOf(type) {
+  return COMMIT_TYPES.get(type) || COMMIT_TYPES.get('default')
+}
+
+function transform(commit) {
+  commit.type = typeOf(commit.type)
+  commit.shortHash = commit.hash.substring(0, 7)
+  console.dir(commit, {depth: 20})
+  return commit
+}
+
 module.exports = {
   branches: ['main']
+, parserOpts: {
+    noteKeywords: ['BREAKING', 'BREAKING CHANGE', 'BREAKING CHANGES']
+  , referenceActions: [
+      'close', 'closes', 'closed'
+    , 'fix', 'fixes', 'fixed'
+    , 'resolve', 'resolves', 'resolved'
+    , 'ref', 'Ref'
+    ]
+  }
+, writerOpts: {transform}
+, releaseRules: [
+    {type: 'build', release: 'patch'}
+  , {type: 'ci', release: 'patch'}
+  , {type: 'chore', release: 'patch'}
+  , {type: 'refactor', release: 'patch'}
+  , {type: 'test', release: 'patch'}
+  , {type: 'doc', release: 'patch'}
+  , {type: 'perf', release: 'minor'}
+  ]
 , plugins: [
-    ['@semantic-release/commit-analyzer', {
-      parserOpts: {
-        noteKeywords: ['BREAKING', 'BREAKING CHANGE', 'BREAKING CHANGES']
-      , referenceActions: [
-          'close', 'closes', 'closed'
-        , 'fix', 'fixes', 'fixed'
-        , 'resolve', 'resolves', 'resolved'
-        , 'ref', 'Ref'
-        ]
-      }
-    , releaseRules: [
-        {type: 'build', release: 'patch'}
-      , {type: 'ci', release: 'patch'}
-      , {type: 'chore', release: 'patch'}
-      , {type: 'refactor', release: 'patch'}
-      , {type: 'test', release: 'patch'}
-      , {type: 'doc', release: 'patch'}
-      , {type: 'perf', release: 'minor'}
-      ]
-    }],
+    ['@semantic-release/commit-analyzer', null],
   , ['@semantic-release/release-notes-generator', null]
   , ['@semantic-release/changelog', {
       changelogTitle: '# Changlog'
