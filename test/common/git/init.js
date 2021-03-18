46538067a0760bc17f654383b8183d4f6de22bf4
Eric Satterwhite <esatterwhite@wi.rr.com>
2021-03-17 23:04:12 -0500

    fix(bin): update workspace handling for latest versions
    the workspaces function was replaced in multi-semantic-release,
    this fixes the cli to utilize the correct functions to collect package
    names

--- test/common/git/init.js
+++ test/common/git/init.js
@@ -7,10 +7,12 @@ const execa = require('execa')
 
 module.exports = init
 
-async function init(dir, branch = 'master') {
+async function init(dir, branch = 'main') {
   const cwd = dir || await fs.mkdtemp(path.join(os.tmpdir(), path.sep))
   await execa('git', ['init'], {cwd: cwd})
   await execa('git', ['checkout', '-b', branch], {cwd: cwd})
   await execa('git', ['config', '--add', 'commit.gpgsign', false])
+  await execa('git', ['config', '--add', 'pull.default', 'current'])
+  await execa('git', ['config', '--add', 'push.default', 'current'])
   return cwd
 }
